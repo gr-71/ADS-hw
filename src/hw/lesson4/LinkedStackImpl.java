61fe494fcb21908232cfb1af875c15c6d9754b49
grybentsov <grybentsov@gmail.com>
2020-11-04 11:31:42 +0300

    update ignore and lesson

---
+++ src/hw/lesson4/LinkedStackImpl.java
@@ -0,0 +1,37 @@
+package hw.lesson4;
+
+import hw.lesson3.stack.Stack;
+
+public class LinkedStackImpl<E> implements Stack<E> {
+
+    private LinkedList<E> data;
+
+    public LinkedStackImpl() {
+        this.data = new SimpleLinkedListImpl<>();
+    }
+
+    @Override
+    public void push(E value) {
+        data.insertFirst(value);
+    }
+
+    @Override
+    public E pop() {
+        return data.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return data.getFirst();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+}
---
+++ src/hw/lesson4/TwoSideLinkedList.java
@@ -0,0 +1,6 @@
+package hw.lesson4;
+
+public interface TwoSideLinkedList<E> extends LinkedList<E> {
+
+    void insertLast(E value);
+}
